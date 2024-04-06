706627a6c212f330734e079d93b46d30594ee7f0
bzakl <143162744+bzakl1@users.noreply.github.com>
2024-04-06 10:09:54 +0530

    added persist

--- src/app/lib/store/index.ts
+++ src/app/lib/store/index.ts
@@ -1,17 +1,49 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import CartReducer, { CartState } from "./slices/cart";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import checkoutCartSlice, {
   CheckoutState,
 } from "@/app/Molecules/store/slice/cart";
 
-export const makeStore = () => {
-  return configureStore({
-    reducer: {
-      cart: CartReducer,
-      checkoutCart: checkoutCartSlice,
-    },
-  });
+const persistConfig = {
+  key: "persist",
+  storage,
 };
+
+const rootReducer = combineReducers({
+  cart: CartReducer,
+  checkoutCart: checkoutCartSlice,
+});
+
+const makeConfiguredStore = () =>
+  configureStore({
+    reducer: rootReducer,
+  });
+
+// export const makeStore = () => {
+//   return configureStore({
+//     reducer: {
+//       cart: CartReducer,
+//       checkoutCart: checkoutCartSlice,
+//     },
+//   });
+// };
+
+export const makeStore = () => {
+  const isServer = typeof window === "undefined";
+  if (isServer) {
+    return makeConfiguredStore();
+  } else {
+    const persistedReducer = persistReducer(persistConfig, rootReducer);
+    let store: any = configureStore({
+      reducer: persistedReducer,
+    });
+    store.__persistor = persistStore(store);
+    return store;
+  }
+};
+
 export type AppStore = ReturnType<typeof makeStore>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<AppStore["getState"]>;
